00e57d3288dcd889d43d830d5659a543bb14e428
Dell <Dell@DESKTOP-34V6QRR>
2020-04-14 19:49:17 +0530

    retrieving data from database

--- src/com/sbits/hibernate/demo/QueryStudentDemo.java
+++ src/com/sbits/hibernate/demo/QueryStudentDemo.java
@@ -43,6 +43,20 @@ public class QueryStudentDemo {
 			displayStudents(theStudents);
 			
 			
+			//query students where lname='v' or fname='sizu'
+			theStudents=session.createQuery("from Student s where s.lname='v' or s.fname='sizu'").list();
+			
+			System.out.println("\n\n students who have last name 'k' or first name 'sizu'");
+			displayStudents(theStudents);
+			
+			
+			//query students where students like '@gmail.com'
+			theStudents=session.createQuery("from Student s where s.email LIKE '%gmail.com'").getResultList();
+			
+			System.out.println("\n\n students who have email end with @gmail.com");
+			displayStudents(theStudents);
+			
+			
 			//commit the transaction
 			session.getTransaction().commit();
 			
